03903912b3125f938599bc21c9883cb7c7928fc1
Fateme <feraghi.fateme@gmail.com>
2021-04-22 21:39:54 -0400

    Read and write the collection of articles in the files.

--- src/BibCreator.java
+++ src/BibCreator.java
@@ -8,6 +8,15 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.StringTokenizer;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class BibCreator {
 
@@ -26,14 +35,15 @@ public class BibCreator {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+
         System.out.println("Welcome to BibCreator!\n");
 //		readFile2();
 		readFromFile();
-		displayFile();
+//		displayFile();
 
 	}
-
-    private static void displayFile() throws IOException {
+	
+	private static void displayFile() throws IOException {
 		// TODO Auto-generated method stub
     		int i = 2;
             Scanner kb = new Scanner(System.in);
@@ -70,212 +80,159 @@ public class BibCreator {
     		}     
 	}
 
-//	void readFromFile() {
-//		for(int i = 1; i < 11; i++) 
-//		{
-//			System.out.println();
-//			BufferedReader br = new BufferedReader(new FileReader("Latex" + i + ".bib"));//FileReader
-//			System.out.println("Latex" + i + ".bib");
-//			
-//			int count = 0;
-//			StringBuilder sb = new StringBuilder();
-//			String line;
-//			try {
-//				line = br.readLine();
-//			} catch (IOException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-//			while (line != null)
-//			{
-//				count++;
-//		        String[] part = line.split("=");
-//		        String endBrace = part[part.length - 1];
-//		        boolean isBrace = false;
-//		        for (int j = 0; j < endBrace.length(); j++) {
-//		        	if(endBrace.charAt(i) == '{')
-//		        	{
-//		        		isBrace = true;
-//		        	
-//		        	}else if(isBrace){
-//		        		++count;
-//		        	}
-//	        		else if(endBrace.charAt(i) == '}'){
-//	        			isBrace = false;
-//	        		}					
-//				}
-//		        
-//		        if(count == 0)
-//		        {
-//		        	System.out.println("Error");
-//		        }
-//		        System.out.println(part);
-//		        line = br.readLine();
-//			}
-//		}
-//			
-//	}
-
-//	public static void readFile2() {
-//
-//		for (int i = 1; i < 11; i++) {
-//
-//			File fileId = new File("Latex" + i + ".bib");
-//
-//			try {
-//				Scanner data = new Scanner(fileId);
-//				int lineNumber = 1;
-//				while (data.hasNextLine()) {
-//
-//					String oneLine = data.nextLine();
-//					System.out.println("Line " + lineNumber + " :" + oneLine);
-//					lineNumber++;
-//
-//				}
-//			} catch (FileNotFoundException e) {
-//				// TODO Auto-generated catch block
-//				return;
-//			}
-//		}
-//	}
-
 	public static void readFromFile() {
-		int count = 0;
-		try {
-			for (int i = 1; i < 11; i++) {
-				File fileId = new File("Latex" + i + ".bib");
-				if (!fileId.exists()) {
-					throw new FileInvalidException("Could not open input file Latex"+i+".bib for reading. \n\nPlease check if file exists! Program will terminate after closing any opened files.\n");
-				}
-				try {
-					Scanner data = new Scanner(fileId);
-					String error = checkEmptyField(data);
-					if (error != null) {
-						count++;
-						throw new FileInvalidException("Error: Detected Empty Field!\n============================\n\nProblem detected with file Latex" + i + ".bib\n" + "File is Invalid: Field \"" + error + "\" is Empty. Processing stopped at this point. Other empty fields may be present as well!\n");
-					}
 
-					ArrayList<JournalClass> journals = new ArrayList<JournalClass>();
-					JournalClass journal = new JournalClass();
-
-					while (data.hasNextLine()) {
-						if (!data.equals("")) {
-							String oneLine = data.nextLine();
-							StringTokenizer st = new StringTokenizer(oneLine, ",");
-							String value = st.nextToken();
-							String[] line = value.split("=");
-							if (value.equals("}")) {
-								writeTofiles(i,journal);
-								journals.add(journal);
-								journal = new JournalClass();
-							}else {
-								journal = parseJournal(line, journal);
-								}
-							}
-						}
-					data.close();
-
-					} catch (Exception e) {
-						System.out.println(e.getMessage());
-						}
-				}
-
-			}	
-		catch (FileInvalidException e) {
-			// TODO: handle exception
+//		int count = 0;
+//		try {
 		
-			System.out.println(e.getMessage());
+		for (int i = 1; i < 11; i++) {
+			File fileId = new File("Latex" + i + ".bib");
+			
+//			if (!fileId.exists()) {
+//				throw new FileInvalidException("Could not open input file Latex"+i+".bib for reading. \n\nPlease check if file exists! Program will terminate after closing any opened files.\n");
+//			}
+			
+			try {
+				Scanner data = new Scanner(fileId);
+				
+//				String error = checkEmptyField(data);
+//				if (error != null) {
+//					count++;
+//					throw new FileInvalidException("Error: Detected Empty Field!\n============================\n\n"
+//							+ "Problem detected with file Latex" + i + ".bib\n" + "File is Invalid: Field \"" 
+//							+ error + "\" is Empty. Processing stopped at this point. Other empty fields may be present as well!\n");
+//				}
+				
+				JournalClass journal = new JournalClass();
+				ArrayList<JournalClass> journals = new ArrayList<JournalClass>();
+				
+				while (data.hasNextLine()) {
+					String oneLine = data.nextLine().trim();
+					StringTokenizer st = new StringTokenizer(oneLine, ",");
+					if (st.hasMoreTokens()) {
+						String value = st.nextToken();
+						String[] line = value.split("=");
+						if (value.equals("}")) {
+							journals.add(journal);
+							journal = new JournalClass();
+						} else {
+							journal = parseJournal(line, journal);
+						}
+					}
+//				data.close();
+				}
+				writeTofiles(i, journals);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
 			}
-		System.out.println("A total of "+ count + " files were invalid, and could not be processed. All other " + (10 - count) + " \"Valid\" files have been created.\n");
 		}
+		}
+//	}catch (FileInvalidException e) {
+//			// TODO: handle exception
+//		
+//			System.out.println(e.getMessage());
+//			}
+//		System.out.println("A total of "+ count + " files were invalid, and could not be processed. All other " + (10 - count) + " \"Valid\" files have been created.\n");
+//		}
+		
 
 	private static String checkEmptyField(Scanner data) {
-	// TODO Auto-generated method stub
-		while (data.hasNextLine()) {
-	        String line = data.nextLine();
-	        if (line.contains("{}")) { 
-				StringTokenizer st = new StringTokenizer(line, "=");
-	            return st.nextToken();
-	        }
-	    }
-		return null;
+		// TODO Auto-generated method stub
+			while (data.hasNextLine()) {
+		        String line = data.nextLine();
+		        if (line.contains("{}")) { 
+					StringTokenizer st = new StringTokenizer(line, "=");
+		            return st.nextToken();
+		        }
+		    }
+			return null;
 	}
+	
 
 	public static JournalClass parseJournal(String[] data, JournalClass journal) {
 
 		if (data.length > 1) {
 			switch (data[0]) {
 			case AUTHOR_KEY: {
-				journal.setAuthor(data[1]);
+				journal.setAuthor(data[1].replace("{", "").replace("}", ""));
 				break;
 			}
 			case JOURNAL_KEY: {
-				journal.setJournal(data[1]);
+				journal.setJournal(data[1].replace("{", "").replace("}", ""));
 				break;
 			}
 			case TITLE_KEY: {
-				journal.setTitle(data[1]);
+				journal.setTitle(data[1].replace("{", "").replace("}", ""));
 				break;
 			}
 			case YEAR_KEY: {
-				journal.setYear(data[1]);
+				journal.setYear(data[1].replace("{", "").replace("}", ""));
 				break;
 			}
 			case VOLUME_KEY: {
-				journal.setVolume(data[1]);
+				journal.setVolume(data[1].replace("{", "").replace("}", ""));
 				break;
 			}
 			case NUMBER_KEY: {
-				journal.setNumber(data[1]);
+				journal.setNumber(data[1].replace("{", "").replace("}", ""));
 				break;
 			}
 			case PAGES_KEY: {
-				journal.setPages(data[1]);
+				journal.setPages(data[1].replace("{", "").replace("}", ""));
 				break;
 			}
 			case KEYWORD_KEY: {
-				journal.setKeyword(data[1]);
+				journal.setKeyword(data[1].replace("{", "").replace("}", ""));
 				break;
 			}
 			case DOI_KEY: {
-				journal.setDoi(data[1]);
+				journal.setDoi(data[1].replace("{", "").replace("}", ""));
 				break;
 			}
 			case ISSN_KEY: {
-				journal.setISSN(data[1]);
+				journal.setISSN(data[1].replace("{", "").replace("}", ""));
 				break;
 			}
 			case MONTH_KEY: {
-				journal.setMonth(data[1]);
+				journal.setMonth(data[1].replace("{", "").replace("}", ""));
 				break;
 			}
 			}
 		}
 		return journal;
 	}
-	
-	public static void writeTofiles(int i,JournalClass journal) {
+
+	public static void writeTofiles(int i, ArrayList<JournalClass> journals) {
 		BufferedWriter outputIEEE = null;
 		BufferedWriter outputACM = null;
 		BufferedWriter outputNJ = null;
 
 		try {
-			File fileIEEE = new File("IEEE"+i+".json");
-			File fileACM = new File("ACM"+i+".json");
-			File fileNJ = new File("NJ"+i+".json");
+			File fileIEEE = new File("IEEE" + i + ".json");
+			File fileACM = new File("ACM" + i + ".json");
+			File fileNJ = new File("NJ" + i + ".json");
 
 			outputIEEE = new BufferedWriter(new FileWriter(fileIEEE));
 			outputACM = new BufferedWriter(new FileWriter(fileACM));
 			outputNJ = new BufferedWriter(new FileWriter(fileNJ));
+			String ieee = "";
+			String acm = "";
+			String nj = "";
 
-			outputIEEE.write(journal.toIEEEString());
-			outputACM.write(journal.toACMString());
-			outputNJ.write(journal.toNJString());
+			for (int j = 0; j < journals.size(); j++) {
+				ieee += journals.get(j).toIEEEString();
+				acm += journals.get(j).toACMString();
+				nj += journals.get(j).toNJString();
+			}
 
-		}catch(IOException e) {
+			outputIEEE.write(ieee);
+			outputACM.write(acm);
+			outputNJ.write(nj);
+
+		} catch (IOException e) {
 			e.printStackTrace();
-		}finally {
-			if(outputIEEE != null) {
+		} finally {
+			if (outputIEEE != null) {
 				try {
 					outputIEEE.close();
 					outputACM.close();
